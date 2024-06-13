38b75259f12a18395fc9ee8564119dcfef514fd5
Kezia Natalia Wongkar <keziawongkar25@gmail.com>
2024-06-14 01:10:34 +0700

    dashboardd

--- Admin/Dashboard/includes/header.php
+++ Admin/Dashboard/includes/header.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <nav>
             <div class="logo">
-                <a href="#">My Dashboard</a>
+                <a href="#">Carelink</a>
             </div>
             <ul class="nav-links">
                 <li><a href="#">Home</a></li>
