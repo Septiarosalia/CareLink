607f7b2adc2490279fd4c85012c1472645fb0952
Kezia Natalia Wongkar <keziawongkar25@gmail.com>
2024-06-14 00:04:18 +0700

    dashboard sisa styling

---
+++ Admin/Dashboard/includes/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+    <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Users</a></li>
+        <li><a href="#">Settings</a></li>
+        <li><a href="#">Logout</a></li>
+    </ul>
+</aside>
---
+++ Admin/Dashboard/index.php
@@ -0,0 +1,9 @@
+<?php include('includes/header.php'); ?>
+<div class="container">
+    <?php include('includes/sidebar.php'); ?>
+    <div class="main-content">
+        <h1>Dashboard</h1>
+        <p>Welcome to the dashboard!</p>
+    </div>
+</div>
+<?php include('includes/footer.php'); ?>
